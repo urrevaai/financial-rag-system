29cec38ee6e2325589e9dd50273118397971aca5
S M Zubeen <128781115+urrevaai@users.noreply.github.com>
2025-08-15 21:54:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 import os
 import time
@@ -179,3 +183,4 @@ def main():
 
 if __name__ == "__main__":
     main()
+
